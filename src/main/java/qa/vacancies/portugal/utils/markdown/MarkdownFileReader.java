e5dafbc1f684684ca4b4d0b783897c209345e8f5
sergiomartins8 <sergiomartins8@ua.pt>
2020-09-06 09:58:33 +0100

    :fire: remove unused piece of code

--- src/main/java/qa/vacancies/portugal/utils/markdown/MarkdownFileReader.java
+++ src/main/java/qa/vacancies/portugal/utils/markdown/MarkdownFileReader.java
@@ -7,6 +7,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class MarkdownFileReader {
+
+    /**
+     * Private constructor to avoid instantiation.
+     */
+    private MarkdownFileReader() {
+    }
+
     @SneakyThrows(value = IOException.class)
     public static byte[] getMarkdown(String filename) {
         return Files.readAllBytes(Paths.get(filename));
