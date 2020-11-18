b4f07d0a2e50932eddf55177a390ea2c7e466b28
sergiomartins8 <sergiomartins8.dev@gmail.com>
2020-11-18 23:45:35 +0000

    trim

--- src/main/java/qa/vacancies/portugal/pages/ItJobsPage.java
+++ src/main/java/qa/vacancies/portugal/pages/ItJobsPage.java
@@ -38,7 +38,7 @@ public class ItJobsPage implements PageObject<ItJobsPage> {
     }
 
     private String getTitle(SelenideElement element) {
-        return element.$(TITLE_SELECTOR).getText();
+        return element.$(TITLE_SELECTOR).getText().trim();
     }
 
     private String getCompany(SelenideElement element) {
